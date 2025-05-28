5c28897f1e3530b1c1ccf98867cf71d27aa9f021
Darz <54253370+djgraphics28@users.noreply.github.com>
2025-05-28 23:14:13 +0800

    is-12 added seeders in settings

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -55,7 +55,7 @@
                 <flux:menu.separator />
 
                 <flux:menu.radio.group>
-                    <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Account Settings') }}
+                    <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Settings') }}
                     </flux:menu.item>
                 </flux:menu.radio.group>
 
