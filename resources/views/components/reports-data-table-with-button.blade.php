4e47ed317976f4df04d6d9b51412b1eb9c73c0ca
EJD222 <eljdiaz500@gmail.com>
2025-07-16 15:09:59 +0800

    refactor: update table components UI and reuse flux button

--- resources/views/components/reports-data-table-with-button.blade.php
+++ resources/views/components/reports-data-table-with-button.blade.php
@@ -7,16 +7,17 @@
     'rowColors' => [],
     'headerBackgroundColor' => 'bg-transparent',
     'emptyMessage' => 'No data available.',
-    'buttonLabel' => 'Select All'
+    'buttonLabel' => 'Select All',
+    'route' => ''
 ])
 
-<div class="bg-white rounded-xl dark:bg-zinc-900 {{ $styleAttributes }}">
+<div class="bg-white rounded-md dark:bg-zinc-900 shadow {{ $styleAttributes }}">
      <div class="mb-4 p-4 bg-gray-50 rounded-t-md">
         <h2 class="text-xl font-bold  text-(--color-accent) dark:text-white">{{ $title }}</h2>
         <p class="text-sm text-gray-500 dark:text-gray-400">{{ $description }}</p>
     </div>
 
-    <div class="overflow-auto rounded-sm dark:border-zinc-700 px-6 pb-6">
+    <div class="overflow-auto rounded-sm dark:border-zinc-700 px-6">
         <table class="min-w-full divide-y divide-gray-200 dark:divide-zinc-700">
             <thead class="{{ $headerBackgroundColor }} text-black text-left text-sm">
                 <tr>
@@ -59,10 +60,11 @@
         </table>
     </div>
     
-    <div class="mt-6 text-right pr-6 pb-3">
-        <a href="{{ route('agingreports') }}"
-           class="inline-flex items-center font-medium px-4 py-2 bg-(--color-accent) text-white rounded hover:bg-(--color-accent-alt) dark:bg-indigo-500 dark:hover:bg-indigo-600">
-            {{ $buttonLabel }}
-        </a>
+    <div class="mt-6 text-right pr-6">
+        <div class="flex justify-end mb-4 md:mb-0">
+            <a href="{{ route($route) }}">
+                <flux:button variant="primary">{{ $buttonLabel }}</flux:button>                                
+            </a>
+        </div>
     </div>
 </div>
--- resources/views/components/reports-data-table-with-status.blade.php
+++ resources/views/components/reports-data-table-with-status.blade.php
@@ -6,14 +6,15 @@
     'rowColors' => [],
     'headerBackgroundColor' => 'bg-transparent',
     'emptyMessage' => 'No data available.',
-    'buttonLabel' => 'See All'
+    'buttonLabel' => 'See All',
+    'route' => ''
 ])
 
 @php
     $statusIndex = array_search('Status', $headers);
 @endphp
 
-<div class="bg-white rounded-xl dark:bg-zinc-900">
+<div class="bg-white rounded-md dark:bg-zinc-900 shadow">
        <div class="mb-4 p-4 bg-gray-50 rounded-t-md">
         <h2 class="text-xl font-bold  text-(--color-accent) dark:text-white">{{ $title }}</h2>
         <p class="text-sm text-gray-500 dark:text-gray-400">{{ $description }}</p>
@@ -48,7 +49,7 @@
                                         if (strtolower($cell) === 'overdue') {
                                             $statusClasses = 'bg-[#FFEAE8] capitalize text-[var(--color-accent-2)] px-3 py-1 font-semibold rounded-3xl';
                                         } elseif (strtolower($cell) === 'pending') {
-                                            $statusClasses = 'bg-yellow-100 capitalize text-yellow-400 px-3 py-1 font-semibold rounded-3xl';
+                                            $statusClasses = 'bg-(--color-yellow-100) capitalize text-(--color-yellow-400) px-3 py-1 font-semibold rounded-3xl';
                                         } elseif (strtolower($cell) === 'paid') {
                                             $statusClasses = 'bg-green-100 capitalize text-green-400 px-3 py-1 font-semibold rounded-3xl';
                                         } elseif (strtolower($cell) === 'cancelled') {
@@ -78,9 +79,10 @@
     </div>
 
     <div class="mt-6 text-right pr-6 pb-6">
-        <a href="{{ route('agingreports') }}"
-           class="inline-flex items-center px-4 py-2 bg-(--color-accent) text-white rounded hover:bg-(--color-accent-alt) dark:bg-indigo-500 dark:hover:bg-indigo-600">
-            {{ $buttonLabel }}
-        </a>
+        <div class="flex justify-end mb-4 md:mb-0">
+            <a href="{{ route($route) }}">
+                <flux:button variant="primary">{{ $buttonLabel }}</flux:button>                                
+            </a>
+        </div>
     </div>
 </div>
