ee01457288067d542bf716497d2602cb1158cbd0
EJD222 <eljdiaz500@gmail.com>
2025-07-16 16:52:09 +0800

    feat: customize create button behavior and label for empty stock view

--- resources/views/livewire/stocks/index.blade.php
+++ resources/views/livewire/stocks/index.blade.php
@@ -20,19 +20,20 @@ new class extends Component {
     public $brand = '';
     public $unit = '';
     public $productType = '';
-    public $perPage = 10;
+    public $perPage = 5;
 
     public function mount()
     {
-        $this->perPage = session('perPage', 10);
+        $this->perPage = session('perPage', 5);
     }
 
     public function updatedPerPage($value)
     {
+        $this->perPage = $value;
         session(['perPage' => $value]);
         $this->resetPage();
     }
-
+    
     public function updatedSearch()
     {
         $this->resetPage();
@@ -113,6 +114,13 @@ new class extends Component {
         :items="$stocks"
         searchPlaceholder="Search Products/Stocks..."
         message="No stocks available."
+        createButtonLabel="Receive Stock"
+        createButtonAbility="products.create"
+        createButtonRoute="stocks.create"
+        :showNewCreateButtonIfEmpty="true"
+        createButtonLabelIfEmpty="Add Product"
+        createButtonAbilityIfEmpty="products.create"
+        createButtonRoute="stocks.create"
     >
         <x-slot:emptyIcon>
             <svg class="w-48 h-48 mb-2 text-gray-300 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
