67f97375ef7780a7deb075c3635c1ea492ccf5a1
EJD222 <eljdiaz500@gmail.com>
2025-07-21 11:31:18 +0800

    feat: create UnitsForm component

---
+++ resources/views/components/units-form.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'isEditing' => false,
+])
+
+<div>
+    <form wire:submit="save">
+        <div class="bg-white dark:bg-gray-900 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="mb-4">
+                <flux:input wire:model="name" :label="__('Name')" type="text"
+                    class="dark:bg-gray-800 dark:text-gray-100 dark:border-gray-600" />
+            </div>
+        </div>
+        <div class="bg-gray-50 dark:bg-gray-800 px-4 py-3 sm:px-6 sm:flex sm:justify-end sm:space-x-2 space-y-2 sm:space-y-0 flex flex-col sm:flex-row">
+            <flux:button class="sm:w-auto" variant="danger" wire:click="cancel">Cancel</flux:button>
+            <flux:button class="sm:w-auto" type="submit" variant="primary">
+                {{ $isEditing ? 'Update' : 'Create' }}
+            </flux:button>
+        </div>
+    </form>
+</div>
