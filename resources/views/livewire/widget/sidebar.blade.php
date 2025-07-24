8fbe96e6acd8d3a05781e3023b163cbda1cc8063
JoshDaguio <bennethpineda@gmail.com>
2025-07-24 19:24:11 +0800

    Hid Purchase Request Nav

--- resources/views/livewire/widget/sidebar.blade.php
+++ resources/views/livewire/widget/sidebar.blade.php
@@ -48,12 +48,12 @@ new class extends Component {
                         'label' => 'Customers',
                         'permission' => 'customers.view',
                     ],
-                    [
-                        'icon' => 'pencil-square',
-                        'route' => 'purchase-requests',
-                        'label' => 'Purchase Requests',
-                        'permission' => 'purchase-requests.view',
-                    ],
+                    // [
+                    //     'icon' => 'pencil-square',
+                    //     'route' => 'purchase-requests',
+                    //     'label' => 'Purchase Requests',
+                    //     'permission' => 'purchase-requests.view',
+                    // ],
                     [
                         'icon' => 'document-text',
                         'route' => 'purchase-orders',
