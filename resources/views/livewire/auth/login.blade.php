37addad1e1d83ecdcebaa1fd0289f236f7bf8e0d
EJD222 <eljdiaz500@gmail.com>
2025-07-25 16:05:01 +0800

    style: update login and register pages with new layout and styles

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -88,17 +88,15 @@ new #[Layout('components.layouts.auth')] class extends Component {
 }; ?>
 
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+    <x-auth-header :title="__('Welcome Back!')" :description="__('Please enter your email and password below to log in.')" />
 
-    <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form wire:submit="login" class="flex flex-col gap-6">
-        <!-- Email Address -->
+    
         <flux:input wire:model="email" :label="__('Username or Email address')" type="text" required autofocus
             placeholder="username / email" />
 
-        <!-- Password -->
         <div class="relative">
             <flux:input wire:model="password" :label="__('Password')" type="password" required
                 autocomplete="current-password" :placeholder="__('Password')" />
@@ -110,16 +108,15 @@ new #[Layout('components.layouts.auth')] class extends Component {
             @endif
         </div>
 
-        <!-- Remember Me -->
         <flux:checkbox wire:model="remember" :label="__('Remember me')" />
 
         <div class="flex items-center justify-end">
-            <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
+            <flux:button variant="primary" color="blue" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
         </div>
     </form>
 
     @if (Route::has('register'))
-        <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
+        <div class="space-x-1 text-center text-xs sm:text-sm text-zinc-600 dark:text-zinc-400">
             {{ __('Don\'t have an account?') }}
             <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
         </div>
